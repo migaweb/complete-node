c93745276d7f555f329f6a31b053f15cd69d4d7d
micke <miga02@gmail.com>
2020-02-23 12:53:54 +0100

    Added handlebars to serve dynamic content in web server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,30 @@ const express = require("express")
 
 const publicDirectoryPath = path.join(__dirname, "../public")
 const app = express();
+app.set("view engine", "hbs")
 app.use(express.static(publicDirectoryPath))
+
+app.get("", (req, res) => {
+    res.render("index", {
+        title: "Weather",
+        name: "Micke"
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About",
+        name: "Micke"
+    })
+})
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        title: "Help",
+        name: "Micke",
+        helpText: "Welcome to the help pages"
+    })
+})
 
 app.get("/weather", (req, res) => {
     res.send({
