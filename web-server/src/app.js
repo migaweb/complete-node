78bb0f5449ec3a19dedd8d0b346438ebe57ea4ba
micke <miga02@gmail.com>
2020-02-23 17:35:30 +0100

    Added styling to weather app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,20 @@
 const path = require("path")
 const express = require("express")
+const hbs = require("hbs")
 
-const publicDirectoryPath = path.join(__dirname, "../public")
 const app = express();
+
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, "../public")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
+
+// Setup handlebars engine and views location
 app.set("view engine", "hbs")
+app.set("views", viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get("", (req, res) => {
@@ -35,6 +46,22 @@ app.get("/weather", (req, res) => {
             summary: "It is cloudy",
             temperature: 9
         }
+    })
+})
+
+app.get("/help/*", (req, res) => {
+    res.render("404", {
+        title: "Help article not found",
+        name: "Micke",
+        errorMessage: "The help article you are looking for could not be found."
+    })
+})
+
+app.get("*", (req, res) => {
+    res.render("404", {
+        title: "Page not found",
+        name: "Micke",
+        errorMessage: "The page you are looking for could not be found."
     })
 })
 
