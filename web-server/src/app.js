06c17a242c5716888182f822ecc645b4744cf90d
micke <miga02@gmail.com>
2020-02-23 10:56:30 +0100

    Added public area to the web server.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,18 @@
+const path = require("path")
 const express = require("express")
-const app = express();
 
-app.get("", (req, res) => {
-    res.send("Hello express!")
-})
-app.get("/help", (req, res) => {
-    res.send("Help page")
-})
-app.get("/about", (req, res) => {
-    res.send("About")
-})
+const publicDirectoryPath = path.join(__dirname, "../public")
+const app = express();
+app.use(express.static(publicDirectoryPath))
+
 app.get("/weather", (req, res) => {
-    res.send("<h1>Weather</h1>")
+    res.send({
+        location: "Malmö",
+        forecast: {
+            summary: "It is cloudy",
+            temperature: 9
+        }
+    })
 })
 
 app.listen(3000, () => {
