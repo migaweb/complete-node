f8abee6ef82cd22b30deafde82d5e2ab3218d098
micke <miga02@gmail.com>
2020-02-28 20:44:36 +0100

    Added authentication.

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,9 @@
 const validator = require("validator")
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
-const User = mongoose.model("User", {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -10,6 +12,7 @@ const User = mongoose.model("User", {
     email: {
         type: String,
         required: true,
+        unique:true,
         trim: true,
         lowercase:true,
         validate(value) {
@@ -37,7 +40,53 @@ const User = mongoose.model("User", {
                 throw new Error("Password cannot contain \"password\"")
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+// Hash the plain text password before saving
+userSchema.pre("save", async function(next) {
+    const user = this;
+
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next() // signal ready.
+})
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+
+    const token = jwt.sign( { _id: user._id.toString() }, "migaweb")
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token;
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error("Unable to login")
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error("Unable to login")
+    }
+
+    return user
+}
+
+const User = mongoose.model("User", userSchema)
 
 module.exports = User
