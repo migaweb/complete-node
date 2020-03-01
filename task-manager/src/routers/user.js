59091042ee01d25c38d1223a1dfeba0f49d2dedd
micke <miga02@gmail.com>
2020-03-01 19:12:21 +0100

    Added environment variables

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,6 +3,7 @@ const multer = require("multer")
 const sharp = require("sharp")
 const User = require("../models/user")
 const auth = require("../middleware/auth")
+const { sendWelcomeEmail, sendCancellationEmail } = require("../emails/account")
 
 const router = new express.Router()
 
@@ -10,9 +11,10 @@ const router = new express.Router()
 router.post("/users", async (req, res) => {
     const user = new User(req.body)
     try {
-    await user.save()
-    const token = await user.generateAuthToken()
-    res.status(201).send({user, token})
+        await user.save()
+        sendWelcomeEmail(user.email, user.name)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch (e) {
         res.status(400).send(e)
     }
@@ -81,6 +83,7 @@ router.patch("/users/me", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
     try {
         await req.user.remove()
+        sendCancellationEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
