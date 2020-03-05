a269aaa9c7062590cbbf4249d0f69c2b80345a98
micke <miga02@gmail.com>
2020-03-05 20:20:49 +0100

    Added send message and location to chat

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,10 +1,48 @@
 const socket = io()
 
-socket.on("countUpdated", (count) => {
-    console.log("The count has been updated: ", count)
+//Elements
+const $messageForm = document.querySelector("#message-form")
+const $messageFormInput = $messageForm.querySelector("input")
+const $messgeFormButton = $messageForm.querySelector("button")
+const $sendLocationButton = document.querySelector("#send-location")
+
+socket.on("message", (message) => {
+    console.log(message)
 })
 
-document.querySelector("#increment").addEventListener("click", () => {
-    console.log("Clicked: ")
-    socket.emit("increment")
+$messageForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    $messgeFormButton.setAttribute("disabled", "disabled")
+
+    const message = e.target.elements.message.value
+    socket.emit("sendMessage", message, (error) => {
+        
+        $messgeFormButton.removeAttribute("disabled")
+        $messageFormInput.value = ""
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+        console.log("Message delivered.")
+    })
+    
+})
+
+$sendLocationButton.addEventListener("click", () => {
+    if (!navigator.geolocation) {
+        return alert("Geolocation is not supported by your browser.")
+    }
+
+    $sendLocationButton.setAttribute("disabled", "disabled")
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const location = {
+            latitude: position.coords.latitude, longitude: position.coords.longitude
+        }
+        socket.emit("sendLocation", location, (message) => {
+            $sendLocationButton.removeAttribute("disabled")
+            console.log(message)
+        })
+    })
 })
